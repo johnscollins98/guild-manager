535036627d11554c51b3b0a5fd6c89fe09d67dc7
John Collins <johnscollins98@gmail.com>
2020-08-10 01:39:37 +0100

    Changes for deployment

--- src/Server/server.js
+++ src/Server/server.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 3001;
 const discordRoute = require("./routes/discord");
 const gw2Route = require("./routes/gw2");
 
-app.use("/discord", discordRoute);
-app.use("/gw2", gw2Route);
+app.use(express.static(path.join(__dirname, "../../build")));
+app.use("/api/discord", discordRoute);
+app.use("/api/gw2", gw2Route);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../../build/index.html"));
+});
 
 app.listen(port, () => console.info(`Listening on port ${port}`));
