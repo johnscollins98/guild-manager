16c984e6530545c933961774ec711777015cf20d
John Collins <johnscollins98@gmail.com>
2021-04-10 12:03:34 +0100

    fix bug in isEventLeader middleware

--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -8,7 +8,7 @@ const isAdmin = async (req, res, next) => {
 
 const isEventLeader = async (req, res, next) => {
   const authInfo = await getUserAuthInfo(req);
-  if (!!authInfo.isEventLeader) return next();
+  if (!!authInfo.isEventLeader || !!authInfo.isAdmin) return next();
   return res.redirect(303, '/forbidden');
 };
 
