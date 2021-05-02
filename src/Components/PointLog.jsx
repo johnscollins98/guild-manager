66746996e3eba58e791c4527d2182c4d9fa61c0a
John Collins <johnscollins98@gmail.com>
2021-05-02 22:00:48 +0100

    add old value/diff to point log

--- src/Components/PointLog.jsx
+++ src/Components/PointLog.jsx
@@ -20,7 +20,9 @@ const PointLog = ({ data, filterString }) => {
         <TableRow>
           <TableCell>Changed by</TableCell>
           <TableCell>Change to</TableCell>
+          <TableCell>Old Point Value</TableCell>
           <TableCell>New Point Value</TableCell>
+          <TableCell>Change</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
@@ -28,7 +30,16 @@ const PointLog = ({ data, filterString }) => {
           <TableRow>
             <TableCell>{entry.givenBy}</TableCell>
             <TableCell>{entry.givenTo}</TableCell>
+            <TableCell>{entry.oldVal || '-'}</TableCell>
             <TableCell>{entry.newVal}</TableCell>
+            <TableCell>
+              {entry.oldVal && entry.newVal ? (
+                <>
+                  {entry.newVal - entry.oldVal > 0 ? '+' : null}
+                  {entry.newVal - entry.oldVal}
+                </>
+              ) : null}
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
