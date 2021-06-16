5b5ff9387355f93fd9a21599b9fa17c8e6f98a6a
John Collins <33354906+johnscollins98@users.noreply.github.com>
2021-06-16 21:30:36 +0100

    move files to a client server structure (#9)

--- Server/routes/forbidden.js
+++ Server/routes/forbidden.js
@@ -4,7 +4,9 @@ const path = require('path');
 router.get('/', (req, res) => {
   res
     .status(403)
-    .sendFile(path.join(__dirname, '..', '..', 'build', 'forbidden.html'));
+    .sendFile(
+      path.join(__dirname, '..', '..', 'Client', 'build', 'forbidden.html')
+    );
 });
 
 module.exports = router;
