63c0ed05cfdd5a0a52ae5a055b3404e20a2d7b01
johnscollins98 <johnscollins98@gmail.com>
2021-06-30 09:18:12 +0100

    convert  log to ts

--- src/Components/Log.tsx
+++ src/Components/Log.tsx
@@ -1,15 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+
+import './Log.scss';
+
+import GW2LogEntry from '../Interfaces/GW2LogEntry';
 
 import { filterLogByString } from '../utils/Helpers';
-import './Log.scss';
+import { fetchGW2Log } from '../utils/DataRetrieval';
 import LogEntry from './LogEntry';
 import LoaderPage from './LoaderPage';
-import { useQuery } from 'react-query';
-import { fetchGW2Log } from '../utils/DataRetrieval';
 
-const Log = ({ filterString, openToast }) => {
-  const [filteredData, setFilteredData] = useState([]);
+import { useQuery } from 'react-query';
+
+interface Props {
+  filterString: string;
+  openToast: (msg: string, status: string) => void;
+}
+
+const Log = ({ filterString, openToast }: Props) => {
+  const [filteredData, setFilteredData] = useState<GW2LogEntry[]>([]);
 
   const { isLoading, data, error } = useQuery('gw2log', () => fetchGW2Log());
 
@@ -31,14 +39,6 @@ const Log = ({ filterString, openToast }) => {
       ))}
     </div>
   );
-};
-
-Log.propTypes = {
-  /* String to filter data */
-  filterString: PropTypes.string.isRequired,
-
-  /* Function to open toast */
-  openToast: PropTypes.func.isRequired
 };
 
 export default Log;
