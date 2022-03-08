780145c7e5625876fb3c27f708800d684cefe98e
johnscollins98 <johnscollins98@gmail.com>
2022-03-08 11:57:51 +0000

    improve give warning form style

--- Client/src/Components/WarningsViewer.tsx
+++ Client/src/Components/WarningsViewer.tsx
@@ -41,28 +41,30 @@ const WarningsViewer = ({ isOpen, onClose, onDeleteWarning, member }: Props) => 
   );
 
   return (
-    <Dialog open={isOpen} onClose={onClose} fullWidth maxWidth={false}>
+    <Dialog open={isOpen} onClose={onClose} maxWidth={false}>
       <DialogTitle>Warnings for {member.memberId}</DialogTitle>
       <DialogContent className="warning-content">
         {member.warnings.map((warning) => (
           <Card variant="outlined" className="warning-card" key={warning._id}>
-            <span className="date field">
-              <CalendarToday className="icon" />
-              <Typography>{new Date(warning.timestamp).toDateString()}</Typography>
-            </span>
-            <span className="given-by field">
-              <Person className="icon" />
-              <Typography>{warning.givenBy}</Typography>
-            </span>
-            <span className="reason field">
-              <Assignment className="icon" />
-              <Typography>{warning.reason}</Typography>
-            </span>
-            <span className="actions field">
+            <div className="data">
+              <span className="date field">
+                <CalendarToday className="icon" />
+                <Typography>{new Date(warning.timestamp).toDateString()}</Typography>
+              </span>
+              <span className="given-by field">
+                <Person className="icon" />
+                <Typography>{warning.givenBy}</Typography>
+              </span>
+              <span className="reason field">
+                <Assignment className="icon" />
+                <Typography>{warning.reason}</Typography>
+              </span>
+            </div>
+            <div className="actions">
               <IconButton onClick={() => handleDeleteWarning(warning)}>
                 <Close />
               </IconButton>
-            </span>
+            </div>
           </Card>
         ))}
       </DialogContent>
