5b762622ec0a8d692643942b403c2b20fcd3ad5e
John Collins <johnscollins98@gmail.com>
2022-08-26 10:51:58 +0100

    address deployment issues

--- Server/src/config/index.ts
+++ Server/src/config/index.ts
@@ -16,7 +16,7 @@ export const config = {
   discordGuildId: process.env.DISCORD_GUILD_ID,
   gw2guildId: process.env.GW2_GUILD_ID,
   gw2apiToken: process.env.GW2_API_TOKEN,
-  port: process.env.PORT ? parseInt(process.env.PORT) : 5000,
+  port: process.env.PORT,
   atlasUri: process.env.ATLAS_URI,
   discordClientId: process.env.DISCORD_CLIENT_ID,
   discordClientSecret: process.env.DISCORD_CLIENT_SECRET,
