481e450d450a6b463d79c65c6b91f6354d3e4d3d
John Collins <johnscollins98@gmail.com>
2023-03-13 07:59:46 +0000

    remove unnecessary state in Log.tsx

--- Client/src/Components/Log/Log.tsx
+++ Client/src/Components/Log/Log.tsx
@@ -1,9 +1,4 @@
-import { useEffect, useState } from 'react';
-import { useQuery } from 'react-query';
-import GW2LogEntry from '../../Interfaces/GW2LogEntry';
 import { useGW2Log } from '../../utils/apis/gw2-api';
-import { filterLogByString } from '../../utils/Helpers';
-import { useToast } from '../Common/ToastContext';
 import LoaderPage from '../LoaderPage';
 import './Log.scss';
 import LogEntry from './LogEntry';
@@ -13,22 +8,19 @@ interface Props {
 }
 
 const Log = ({ filterString }: Props) => {
-  const [filteredData, setFilteredData] = useState<GW2LogEntry[]>([]);
-  const openToast = useToast();
-
   const { isLoading, data, error } = useGW2Log();
 
-  useEffect(() => {
-    if (!data) return;
-    setFilteredData(filterLogByString(data, filterString));
-  }, [data, filterString]);
-
   if (error) {
-    openToast('There was an error getting the log', 'error');
-    console.error(error);
-    return null;
+    return <>There was an error loading log data.</>;
   }
-  if (isLoading) return <LoaderPage />;
+
+  if (isLoading || !data) return <LoaderPage />;
+
+  const lowerCaseFilterString = filterString.toLowerCase();
+  const filteredData = data.filter(entry =>
+    entry.message.toLowerCase().includes(lowerCaseFilterString)
+  );
+
   return (
     <div className="log-container">
       {filteredData.map(entry => (
