32089c9cb33fc1ef9aa6463223e56861851e3b46
John Collins <johnscollins98@gmail.com>
2023-03-28 17:37:09 +0100

    add eslint setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 import svgrPlugin from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), eslint()],
   server: {
     open: true,
     port: 5001,
