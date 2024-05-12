bd02bf9b8da56d10f065893f62bdedca8ada759f
John Collins <johnscollins98@gmail.com>
2024-05-12 14:57:31 +0100

    add setup for typeorm migrations

---
+++ server/src/dataSource.ts
@@ -0,0 +1,12 @@
+import path from 'path';
+import { DataSource } from 'typeorm';
+import { config } from './config';
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  url: config.databaseUrl,
+  synchronize: false,
+  logging: true,
+  entities: [path.join(__dirname, '**', '*.model.{ts,js}')],
+  migrations: [path.join(__dirname, 'migrations', '*.ts')]
+});
