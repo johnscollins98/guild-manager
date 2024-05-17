a4df1d827d25c09e12d5f889fe5feb72846034a8
John Collins <johnscollins98@gmail.com>
2024-05-17 23:27:30 +0100

    add ts checking to vite dev server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,13 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 import eslint from 'vite-plugin-eslint';
 import svgrPlugin from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), eslint()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), eslint(), checker({ typescript: true })],
   server: {
     open: true,
     port: 3001,
