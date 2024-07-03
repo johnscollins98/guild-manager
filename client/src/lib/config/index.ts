41ad60517ac819fe586b035a6751b5f0f20624c5
John Collins <johnscollins98@gmail.com>
2024-07-03 20:27:28 +0100

    set default urls for backend and frontend base url
    just makes life a tiny bit easier when starting on a new system

--- client/src/lib/config/index.ts
+++ client/src/lib/config/index.ts
@@ -1,4 +1,6 @@
 export const config = {
-  backEndBaseUrl: import.meta.env.PROD ? '' : import.meta.env.VITE_BACKEND_BASE_URL || '',
+  backEndBaseUrl: import.meta.env.PROD
+    ? ''
+    : import.meta.env.VITE_BACKEND_BASE_URL ?? 'http://localhost:3000',
   discordReinviteUrl: import.meta.env.VITE_DISCORD_REINVITE_LINK
 };
