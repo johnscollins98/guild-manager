4de2d7992fdac8b8e03a5f0ef462b79dc6d48951
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2024-12-21 12:58:31 +0000

    Normalize discord names

--- client/src/lib/utils/data-processing.ts
+++ client/src/lib/utils/data-processing.ts
@@ -28,7 +28,8 @@ export const generateGW2RosterRecords = (
         }
 
         const discordName = m.name
-          ?.toLowerCase()
+          ?.normalize('NFKC')
+          .toLowerCase()
           .replace(
             /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
             ''
