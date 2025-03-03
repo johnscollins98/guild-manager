2311a0622970e1626324154f62ee9f2be1ce3bd1
John Collins <johnscollins98@gmail.com>
2025-03-03 19:55:40 +0000

    allow use of shared suspense boundary

--- client/src/components/common/query-boundary.tsx
+++ client/src/components/common/query-boundary.tsx
@@ -1,4 +1,4 @@
-import { type ComponentType, type PropsWithChildren, Suspense } from 'react';
+import { type ComponentType, type Key, type PropsWithChildren, Suspense } from 'react';
 import { type FallbackProps } from 'react-error-boundary';
 import { ErrorBoundary } from './error-boundary';
 import LoaderPage from './loader-page';
@@ -6,15 +6,17 @@ import LoaderPage from './loader-page';
 export interface QueryBoundaryProps extends PropsWithChildren {
   fallback?: React.ReactNode;
   errorRender?: ComponentType<FallbackProps>;
+  key?: Key;
 }
 
 export const QueryBoundary = ({
   children,
+  key,
   fallback = <LoaderPage />,
   errorRender
 }: QueryBoundaryProps) => {
   return (
-    <Suspense fallback={fallback}>
+    <Suspense fallback={fallback} key={key}>
       <ErrorBoundary errorRender={errorRender}>{children}</ErrorBoundary>
     </Suspense>
   );
