57b6e25f250f556136e36a036b9e7c77a5be4c51
John Collins <johnscollins98@gmail.com>
2025-07-07 14:01:40 +0100

    add support for warning log filter string also moves warning list gathering to a hook

--- client/src/components/warning-log/WarningLog.tsx
+++ client/src/components/warning-log/WarningLog.tsx
@@ -1,43 +1,9 @@
 import { Box } from '@mui/material';
-import { useSuspenseQueries } from '@tanstack/react-query';
-import { discordMembersQuery } from '../../lib/apis/discord-api';
-import { warningsQuery } from '../../lib/apis/warnings-api';
 import DiscordLogEntry from '../discord-log/discord-log-entry';
+import { useWarningList } from './useWarningList';
 
 export const WarningLog = () => {
-  const [{ data: warnings }, { data: discordMembers }] = useSuspenseQueries({
-    queries: [warningsQuery, discordMembersQuery]
-  });
-
-  const getMemberName = (id: string) => {
-    const member = discordMembers.find(m => m.id === id);
-
-    return member?.nickname || member?.name || 'Unknown Member';
-  };
-
-  const logEntries = warnings.map(w => {
-    const date = new Date(w.timestamp);
-
-    const givenTo = getMemberName(w.givenTo);
-    const givenBy = getMemberName(w.givenBy);
-
-    const summary = `${w.type.toLocaleUpperCase()} - Given to ${givenTo} by ${givenBy}.`;
-
-    const details = [w.reason];
-
-    if (w.lastUpdatedBy) {
-      const updatedMember = getMemberName(w.lastUpdatedBy);
-      const updatedDate = new Date(w.lastUpdatedTimestamp);
-      details.push(`Last updated on ${updatedDate.toLocaleString()} by ${updatedMember}.`);
-    }
-
-    return {
-      date,
-      summary,
-      details,
-      id: w.id
-    };
-  });
+  const logEntries = useWarningList();
 
   return (
     <Box sx={{ overflowX: 'hidden', overflowY: 'auto' }}>
---
+++ client/src/components/warning-log/useWarningList.ts
@@ -0,0 +1,57 @@
+import { useSuspenseQueries } from '@tanstack/react-query';
+import { useCallback, useMemo } from 'react';
+import { discordMembersQuery } from '../../lib/apis/discord-api';
+import { warningsQuery } from '../../lib/apis/warnings-api';
+import { useFilterString } from '../../lib/utils/use-filter-string';
+
+export const useWarningList = () => {
+  const [{ data: warnings }, { data: discordMembers }] = useSuspenseQueries({
+    queries: [warningsQuery, discordMembersQuery]
+  });
+
+  const filterStringLowerCase = useFilterString().toLowerCase();
+
+  const getMemberName = useCallback(
+    (id: string) => {
+      const member = discordMembers.find(m => m.id === id);
+
+      return member?.nickname || member?.name || 'Unknown Member';
+    },
+    [discordMembers]
+  );
+
+  const logEntries = useMemo(
+    () =>
+      warnings.map(w => {
+        const date = new Date(w.timestamp);
+
+        const givenTo = getMemberName(w.givenTo);
+        const givenBy = getMemberName(w.givenBy);
+
+        const summary = `[${w.type.toLocaleUpperCase()}] Given to ${givenTo} by ${givenBy}.`;
+
+        const details = [w.reason];
+
+        if (w.lastUpdatedBy) {
+          const updatedMember = getMemberName(w.lastUpdatedBy);
+          const updatedDate = new Date(w.lastUpdatedTimestamp);
+          details.push(`Last updated on ${updatedDate.toLocaleString()} by ${updatedMember}.`);
+        }
+
+        return {
+          date,
+          summary,
+          details,
+          id: w.id
+        };
+      }),
+    [getMemberName, warnings]
+  );
+
+  const filtered = useMemo(
+    () => logEntries.filter(e => e.summary.toLowerCase().includes(filterStringLowerCase)),
+    [filterStringLowerCase, logEntries]
+  );
+
+  return filtered;
+};
