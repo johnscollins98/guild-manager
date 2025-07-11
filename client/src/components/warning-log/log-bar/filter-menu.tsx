8025397cdb5e4c879f363f07c42d63dc8834a3e8
John Collins <johnscollins98@gmail.com>
2025-07-11 10:49:37 +0100

    put filter checkboxes inline on larger screens

--- client/src/components/warning-log/log-bar/filter-menu.tsx
+++ client/src/components/warning-log/log-bar/filter-menu.tsx
@@ -1,5 +1,12 @@
 import FilterList from '@mui/icons-material/FilterList';
-import { Checkbox, FormControlLabel, IconButton, MenuItem, Popover } from '@mui/material';
+import {
+  Checkbox,
+  FormControlLabel,
+  IconButton,
+  MenuItem,
+  Popover,
+  type FormControlLabelProps
+} from '@mui/material';
 import { Box } from '@mui/system';
 import { use, useRef, useState } from 'react';
 import { WarningTypeLabels, type WarningType } from 'server';
@@ -12,32 +19,65 @@ export const FilterMenu = () => {
 
   return (
     <>
-      <IconButton onClick={() => setShowPopover(!showPopover)} ref={buttonRef}>
-        <FilterList />
-      </IconButton>
-      <Popover
-        open={showPopover}
-        onClose={() => setShowPopover(false)}
-        anchorEl={buttonRef.current}
-        anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
-      >
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          {Object.entries(warningTypesToDisplay).map(([key, value]) => (
-            <MenuItem
-              key={key}
-              dense
-              onClick={() => setWarningTypesToDisplay({ ...warningTypesToDisplay, [key]: !value })}
-            >
-              <FormControlLabel
-                control={<Checkbox checked={value} />}
-                value={value}
-                onClick={e => e.preventDefault()}
-                label={WarningTypeLabels[key as WarningType]}
-              />
-            </MenuItem>
-          ))}
-        </Box>
-      </Popover>
+      <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
+        <IconButton onClick={() => setShowPopover(!showPopover)} ref={buttonRef}>
+          <FilterList />
+        </IconButton>
+        <Popover
+          open={showPopover}
+          onClose={() => setShowPopover(false)}
+          anchorEl={buttonRef.current}
+          anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+        >
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            {Object.entries(warningTypesToDisplay).map(([key, value]) => (
+              <MenuItem
+                key={key}
+                dense
+                onClick={() =>
+                  setWarningTypesToDisplay({ ...warningTypesToDisplay, [key]: !value })
+                }
+              >
+                <FilterCheckbox warningkey={key} value={value} onClick={e => e.preventDefault()} />
+              </MenuItem>
+            ))}
+          </Box>
+        </Popover>
+      </Box>
+      <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+        {Object.entries(warningTypesToDisplay).map(([key, value]) => (
+          <FilterCheckbox
+            key={key}
+            warningkey={key}
+            value={value}
+            onChange={() => setWarningTypesToDisplay({ ...warningTypesToDisplay, [key]: !value })}
+          />
+        ))}
+      </Box>
     </>
+  );
+};
+
+const FilterCheckbox = ({
+  warningkey,
+  value,
+  onChange,
+  onClick
+}: {
+  warningkey: string;
+  value: boolean;
+  onChange?: FormControlLabelProps['onChange'];
+  onClick?: FormControlLabelProps['onClick'];
+}) => {
+  return (
+    <FormControlLabel
+      control={<Checkbox checked={value} />}
+      value={value}
+      onChange={onChange}
+      onClick={onClick}
+      name={warningkey}
+      id={warningkey}
+      label={WarningTypeLabels[warningkey as WarningType]}
+    />
   );
 };
